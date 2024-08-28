b383f73044f220aaddd6bddef8ace2deed341d61
Huu <hoangduchuuvn@gmail.com>
2024-08-28 11:23:17 +0700

    v2.1 update title name

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1218938841"
+    serviceWorkerVersion: "1800822722"
   }
 });
